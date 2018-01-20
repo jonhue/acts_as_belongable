d641c0c4cd389d95f369d17204b008b3aff7da1b
Jonas Hübotter <jonas.huebotter@gmail.com>
2018-01-20 15:57:36 +0100

    minor bugfixes

--- app/models/concerns/acts_as_belongable/belonger.rb
+++ app/models/concerns/acts_as_belongable/belonger.rb
@@ -10,8 +10,12 @@ module ActsAsBelongable
             end
 
             def belonger name, source_type, options = {}
-                belonger_belongings_with_scope options[:scope] if options.has_key? :scope
-                has_many name.to_sym, through: ( options.has_key? :scope ? "belonger_belongings_#{scope}".to_sym : :belonger_belongings ), source: :belongable, source_type: source_type.to_s
+                if options.has_key? :scope
+                    belonger_belongings_with_scope options[:scope]
+                    through = "belonger_belongings_#{options[:scope]}".to_sym
+                end
+                through ||= :belonger_belongings
+                has_many name.to_sym, through: through, source: :belongable, source_type: source_type.to_s
             end
             def belonger_belongings_with_scope scope
                 has_many "belonger_belongings_#{scope}".to_sym, -> { where(scope: scope.to_s) }, as: :belonger, class_name: 'Belonging', dependent: :destroy
